58e27bc39d7f7fc88897b2e7f73d308d595f3078
Francisco <fcarlosmonteiro@gmail.com>
2024-01-11 22:42:23 -0300

    refactor

--- bokeh-lib/map-chart.py
+++ bokeh-lib/map-chart.py
@@ -3,13 +3,10 @@ from bokeh.plotting import figure, show, output_file
 from bokeh.models import GeoJSONDataSource, HoverTool, TapTool, CustomJS, Div
 from bokeh.layouts import column
 
-# URL do GeoJSON
 url = "https://raw.githubusercontent.com/jonates/opendata/master/arquivos_geoespaciais/unidades_da_federacao.json"
 
-# Carregar o GeoJSON diretamente do URL
 gdf = gpd.read_file(url)
 
-# Adicionar informações de população fictícias com base no censo populacional aproximado
 populacao = {
     'ACRE': '900 mil',
     'ALAGOAS': '3 milhões',
@@ -40,38 +37,28 @@ populacao = {
     'TOCANTINS': '1.6 milhões'
 }
 
-# Mapear os valores de população fictícia para o DataFrame GeoPandas
 gdf['Populacao'] = gdf['NM_ESTADO'].map(populacao)
 
-# Inicializar a saída do Bokeh para um arquivo HTML
 output_file("mapa_do_Brasil_interativo.html")
 
-# Criar uma fonte de dados GeoJSON para Bokeh
 geosource = GeoJSONDataSource(geojson=gdf.to_json())
 
-# Criar a figura Bokeh sem ferramentas
 p = figure(title="Mapa do Brasil", width=800, height=600, background_fill_color="white", tools="")
 
-# Remover logo e barra de ferramentas
 p.toolbar.logo = None
 p.toolbar_location = None
 
-# Adicionar os estados ao gráfico
 states = p.patches("xs", "ys", source=geosource, line_color="black", line_width=0.5, fill_alpha=1)
 
-# Adicionar a ferramenta Hover para mostrar informações sobre os estados
 hover = HoverTool()
 hover.tooltips = [("Estado", "@NM_ESTADO"), ("População", "@Populacao")]
 p.add_tools(hover)
 
-# Criar uma Div para mostrar informações ao clicar em um estado
 info_div = Div(text="", width=400)
 
-# Adicionar a ferramenta Tap para dar zoom e mostrar informações adicionais ao clicar em um estado
 tap = TapTool()
 p.add_tools(tap)
 
-# Criar um JavaScript callback para tratar o evento de clique em um estado
 callback = CustomJS(args=dict(p=p, info_div=info_div), code="""
     var selected_state = cb_data.source.selected.indices[0];
     if (selected_state !== undefined) {
@@ -85,11 +72,8 @@ callback = CustomJS(args=dict(p=p, info_div=info_div), code="""
 
 tap.callback = callback
 
-# Personalizar a aparência do mapa (opcional)
 p.axis.visible = False
 
-# Criar um layout com o mapa e a Div de informações
 layout = column(p, info_div)
 
-# Mostrar o layout
 show(layout)
