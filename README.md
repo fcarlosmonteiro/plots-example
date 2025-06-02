f84328f14eccf47d759c2156f77b2166f033e784
Francisco Carlos <fcarlosmonteiro@gmail.com>
2025-06-02 18:10:44 -0300

    organizing the repo

--- README.md
+++ README.md
@@ -1,18 +1,51 @@
-## plots-example
-Plots example in matplotlib
+# Plots Example
+
+This repository contains examples of different types of plots using Python visualization libraries.
+
+## Features
+
+- Line plots using Matplotlib
+- Bar plots with customizable legends
+- Interactive plots using Plotly
+- Bubble plots using Bokeh
+
+## Setup
+
+1. Clone the repository:
+```bash
+git clone https://github.com/fcarlosmonteiro/plots-example.git
+cd plots-example
+```
+
+2. Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Each plot type has its own script:
+
+- `lineplot.py`: Creates a simple line plot with two fitness functions
+- `barplot.py`: Creates a bar plot showing execution times
+- `barplot-legend.py`: Creates a bar plot with custom legends
+- `line-plotly.py`: Creates an interactive line plot using Plotly
+- `bubbleplot.py`: Creates a bubble plot using Bokeh
+
+## Examples
 
 **Line plot**
-
-![myimage1](https://github.com/fcarlosmonteiro/plots-example/blob/master/plot.png)
+![Line Plot](plot.png)
 
 **Bar plot**
-
-![myimage2](https://github.com/fcarlosmonteiro/plots-example/blob/master/time.png)
+![Bar Plot](time.png)
 
 **Bar plot with legends**
-
-![myimage3](https://github.com/fcarlosmonteiro/plots-example/blob/master/mscore.png)
+![Bar Plot with Legends](mscore.png)
 
 **Line plot with plotly**
+![Interactive Line Plot](newplot.png)
 
-![myimage3](https://github.com/fcarlosmonteiro/plots-example/blob/master/newplot.png)
+## Contributing
+
+Feel free to submit issues and enhancement requests!
--- barplot.py
+++ barplot.py
@@ -1,17 +1,58 @@
-import matplotlib.pyplot as plt; plt.rcdefaults()
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+Bar Plot Example
+This script creates a bar plot comparing execution times for different configurations.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
+def create_bar_plot(configurations, times, output_file="time.png"):
+    """
+    Create a bar plot comparing execution times.
+    
+    Args:
+        configurations (tuple): Configuration numbers
+        times (list): Execution times in seconds
+        output_file (str): Path to save the output plot
+    """
+    # Create figure and axis
+    fig, ax = plt.subplots(figsize=(8, 6))
+    
+    # Create positions for bars
+    y_pos = np.arange(len(configurations))
+    
+    # Create bars
+    plt.bar(y_pos, times, 
+           align='center',
+           alpha=0.8,
+           color='steelblue',
+           width=0.5,
+           edgecolor='black')
+    
+    # Customize the plot
+    plt.xticks(y_pos, configurations)
+    plt.xlabel('Configuration')
+    plt.ylabel('Time (seconds)')
+    plt.title('Execution Time by Configuration')
+    
+    # Add value labels on top of bars
+    for i, v in enumerate(times):
+        ax.text(i, v + 0.1, f'{v:.2f}s',
+                ha='center', va='bottom')
+    
+    # Adjust layout and save
+    plt.tight_layout()
+    fig.savefig(output_file, dpi=300, bbox_inches='tight')
+    plt.show()
 
-execucoes = (1, 2)
-y_pos = np.arange(len(execucoes))
-fitness = [3.06, 12.98]
+def main():
+    # Sample data
+    configurations = (1, 2)
+    execution_times = [3.06, 12.98]
+    
+    create_bar_plot(configurations, execution_times)
 
-#comentario
-plt.bar(y_pos, fitness, align='center', alpha=0.3, color="black", width=0.5)
-plt.xticks(y_pos, execucoes)
-plt.xlabel("""$Configura c \c\~ao$""")
-plt.ylabel('Tempo (seg)')
-fig.savefig("time.png")
-plt.show()
+if __name__ == "__main__":
+    main()
